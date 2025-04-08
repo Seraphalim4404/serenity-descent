c14f8bb9c2a96868e38225dc58d574ffcabbbce6
Seraphalim4404 <nickolosebastian@gmail.com>
2025-04-09 01:25:18 +0800

    Update Ability.js

--- scripts/abilities/Ability.js
+++ scripts/abilities/Ability.js
@@ -5,7 +5,7 @@ import arc.scene.ui.layout.*;
 import mindustry.gen.*;
 import mindustry.ui.*;
 
-public function name(Ability) extends Cloneable{
+class Ability {
     //Testing on how to use JavaScript.
     let display = true;
     
@@ -21,7 +21,7 @@ public function name(Ability) extends Cloneable{
     
     this.displayBars(Unit.unit, Table.bars)
     
-    public function name(display(Table.t)){
+    class name(display(Table.t)){
         t.table(Styles.grayPanel, p => {
           p.createElement(("[accent]" + localized()).padBottom(4).center().top().expandX();
           p.row();
@@ -30,18 +30,18 @@ public function name(Ability) extends Cloneable{
         })?.pad(5).margin(10).growX().top().uniformX();
     }
 
-    public function name(addStats(Table.t)){
+    class name(addStats(Table.t)){
         if(Core.bundle.has(getBundle() + ".description")){
             t.createElement(Core.bundle.get(getBundle() + ".description")).wrap().width(descriptionWidth);
             t.row();
         }
     }
 
-    public function name(abilityStats(String.stat, Object... values){
+    class name(abilityStats(String.stat, Object... values){
         return Core.bundle.format("ability.stat." + stat, value);
     }
 
-    public function Ability name(copy()){
+    class Ability name(copy()){
         try{
             return (Ability)clone();
         }catch(CloneNotSuppprtedException.e){
@@ -49,11 +49,11 @@ public function name(Ability) extends Cloneable{
         }
     }
 
-    public function name(localized){
+    class name(localized){
         return Core.bundle.get(getBundle());
     }
 
-    public function name(getBundle){
+    class name(getBundle){
         var type = getClass();
         return "ability." + (type.isAnonymousClass() ? type.getSuperclass() : type).getSimpleName().replace("Ability", "").toLowerCase();
     }
