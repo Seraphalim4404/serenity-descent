4fa221bc7de32af26aaaf5c3948b56a55c108af7
Seraphalim4404 <nickolosebastian@gmail.com>
2025-04-08 21:44:01 +0800

    Create Ability.js

---
+++ scripts/abilities/Ability.js
@@ -0,0 +1,31 @@
+export mindustry.entities.abilities.ability;
+
+import arc.*;
+import arc.scene.ui.layout.*;
+import mindustry.gen.*;
+import mindustry.ui.*;
+
+function name(Ability) {
+    //Testing on how to use JavaScript.
+    let display = true;
+
+    this.update(Unit.unit){}
+
+    this.draw(Unit.unit)
+
+    this.death(Unit.unit)
+
+    this.created(Unit.unit)
+
+    this.init(UnitType.type)
+
+    this.displayBars(Unit.unit, Table.bars)
+
+    this.display(Table.t)
+        t.table(Styles.grayPanel, p => {
+          p.createElement(("[accent]" + localized()).padBottom(4).center().top().expandX();
+          p.row();
+          p.left().top().defaults().left();
+          addStats(p);
+        })?.pad(5).margin(10).growX().top().uniformX();
+    }
