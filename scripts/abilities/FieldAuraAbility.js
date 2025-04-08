73d7c201b756cb516f6bdf964171ce9555d17aff
Seraphalim4404 <nickolosebastian@gmail.com>
2025-04-08 22:43:49 +0800

    Create FieldAuraAbility.js

---
+++ scripts/abilities/FieldAuraAbility.js
@@ -0,0 +1,17 @@
+export scripts.abilities;
+
+import arc.*;
+import arc.audio.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.scene.ui.layout.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.world.meta.*;
