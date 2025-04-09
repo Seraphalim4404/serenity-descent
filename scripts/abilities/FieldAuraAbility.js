6c2ed8c4d7ef2418dab629bb486568bc3cc66692
Seraphalim4404 <nickolosebastian@gmail.com>
2025-04-09 11:48:03 +0800

    Update FieldAuraAbility.js

--- scripts/abilities/FieldAuraAbility.js
+++ scripts/abilities/FieldAuraAbility.js
@@ -18,7 +18,7 @@ import mindustry.world.meta.*;
 
 import static vars.*;
 
-function name(FieldAuraAbility) extends Ability{
+public function name(FieldAuraAbility) extends Ability{
     
     class float reload = 60f * 4f;
     class float range = 660f;
