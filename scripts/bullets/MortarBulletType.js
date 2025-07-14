560f24b55630d1bf20306fb63a85051c2217edb2
Seraphalim4404 <nickolosebastian@gmail.com>
2025-07-14 18:32:17 +0800

    Update MortarBulletType.js

--- scripts/bullets/MortarBulletType.js
+++ scripts/bullets/MortarBulletType.js
@@ -1,5 +1,6 @@
-const MortarBulletType = extend(BasicBulletType, {
-  splashDamage: 15,
-  lifetime: 60,
-  speed: 7.5 * 1
+const MortarBulletType = extend(ArtilleryBulletType, {
+  splashDamage: 1,
+  lifetime: 60 * 1,
+  speed: 7.5 * 1,
+  testCollision(bullet, build) {
 });
