5783360ef8c7bea9b74989a06189551c6fe331c5
Seraphalim4404 <nickolosebastian@gmail.com>
2025-07-14 14:56:03 +0800

    Update and rename MortarBulletType.js to MortarBulletType.java

---
+++ scripts/bullets/MortarBulletType.java
@@ -0,0 +1,6 @@
+public class MortarBulletType extends BasicBulletType{
+  public float trailMult = 1f, trailSize = 4f;
+
+  public ArtilleryBulletType(float speed, float damage, String bulletSprite){
+        super(speed, damage, bulletSprite);
+  
--- scripts/bullets/MortarBulletType.js
+++
@@ -1,6 +0,0 @@
-class MortarBulletType extends BasicBulletType{
-  class float trailMult = 1f, trailSize = 4f;
-
-  class ArtilleryBulletType(float speed, float damage, String bulletSprite){
-        super(speed, damage, bulletSprite);
-  
